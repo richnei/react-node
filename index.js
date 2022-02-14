766955bc153cdfcf3053d92f13533181a46cbb8b
Richnei <rich.nei@hotmail.com>
2022-02-14 15:44:49 -0300

    app

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+require("dotenv").config();
 
 const videos = [
   "sUm3mvZii6Y",
   "DvwMVIJb0Jk",
   "Zu073TPW3E8",
   "mvMJl_0oQ_8",
-  "mvMJl_0oQ_8",
+  "MMF9T_9DAcs",
 ];
 
 app.get("/api/react-node", (req, res) => {
   res.send(videos);
 });
 
-app.use(express.static(path.join(__dirname, "front/build")));
+if (process.env.NODE_ENV != "development") {
+  app.use(express.static(path.join(__dirname, "front/build")));
 
-app.get("*", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "front/build/index.html", function (error) {
-      if (error) {
-        res.status(500).send(error);
-      }
-    })
-  );
-});
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.join(__dirname, "front/build/index.html", function (error) {
+        if (error) {
+          res.status(500).send(error);
+        }
+      })
+    );
+  });
+}
 
 app.listen(3000, () => {
   console.log("Running");
